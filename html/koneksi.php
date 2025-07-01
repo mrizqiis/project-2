3eb4e786110b2281b37b70748cdb2d87201b0011
rizqi <muhamadrizqi200105@gmail.com>
2025-07-01 13:21:10 +0000

    5

--- html/koneksi.php
+++ html/koneksi.php
@@ -5,10 +5,7 @@
 	$pass = "1234";	
 	$db = "alimrugi";
 	
-
-
 	$konekdb = new mysqli($host,$user,$pass,$db);
-
 	if ($konekdb->connect_error) {
 		die("Koneksi ke database gagal: " . $konekdb->connect_error);
 	}
